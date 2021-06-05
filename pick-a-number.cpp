c4801c694554a746c9e7c723ac7deb11589152a3
andersonclementte <anderson.clementte@gmail.com>
2021-06-05 11:29:34 -0300

    score system

--- pick-a-number.cpp
+++ pick-a-number.cpp
@@ -7,22 +7,37 @@ int main(){
     cout << "************" << endl;
 
     const int SECRET_NUMBER = 42;
-    //cout << "O numero secreto é " << SECRET_NUMBER << ". Não conte pra ningém" << endl;
-    int guess;
-    cout << "Escolhe um numero, digita ae: " << endl;
-    cin >> guess;
-    cout << "O valor do chute é: " << guess << endl;
+    
+    int tries = 0;
+    bool not_hit = true;
+    double score = 1000.0;
 
-    bool hit = guess == SECRET_NUMBER;
-    bool greater = guess > SECRET_NUMBER;
+    while(not_hit){
+        tries++;
+        int guess;
+        cout << "Tentativa " << tries << endl;
+        cout << "Escolhe um numero, digita ae: " << endl;
+        cin >> guess;
+        cout << "O valor do chute é: " << guess << endl;
 
-    if (hit){
-        cout << "Acerto mizeravi!" << endl;
+        bool hit = guess == SECRET_NUMBER;
+        bool greater = guess > SECRET_NUMBER;
+        double looses = abs(guess - SECRET_NUMBER)/2.0;
+        score = score - looses;
+
+        if (hit){
+            cout << "Acerto mizeravi!" << endl;
+            not_hit = false;
+        }
+        else if (greater){
+            cout << "Chuve foi maior que o número" << endl;
+        }
+        else {
+            cout << "Chute menor do que o número" << endl;
+        }
     }
-    else if (greater){
-        cout << "Chuve foi maior que o número" << endl;
-    }
-    else {
-        cout << "Chute menor do que o número" << endl;
-    }
+
+    cout << "Fim de jogo!\nVocê acertou em " << tries << " tentativas." << endl;
+    cout << "Sua pontuação foi " << score << endl;
+    
 }
